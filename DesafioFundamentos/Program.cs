14ef6ee86e4dc007154ae429c22a0fcd273d8bf3
Jeferson <jeferson143@hotmail.com>
2024-01-11 19:31:12 -0300

    Atualizando

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -8,7 +8,7 @@ decimal precoPorHora = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+precoInicial = Convert.ToDecimal(Console.ReadLine());.
 
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
